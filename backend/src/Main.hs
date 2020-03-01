18b1b8c1a9c7d0547549bc4667aa3eeec9a18dc7
Tom Busby <tom@busby.ninja>
2020-03-01 21:45:46 +0100

    Allow serving index.html from /

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -1,15 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Servant
+import Servant.Server.StaticFiles
 import Network.Wai.Handler.Warp (run)
+import WaiAppStatic.Storage.Filesystem
+import WaiAppStatic.Types (StaticSettings(..), unsafeToPiece)
 
 type API = Raw
 
 server :: Server API
-server = serveDirectoryWebApp "frontend/"
+server = serveDirectoryWith settings
+  where
+    settings = (defaultWebAppSettings "frontend/") {
+        ssIndices = map unsafeToPiece ["index.html"]
+      }
 
 app :: Application
 app = serve (Proxy :: Proxy API) server
 
 main :: IO ()
 main = do
-    let portNum = 8000
-    run portNum app
+  let portNum = 8000
+  putStrLn $ "Server running on " ++ show portNum
+  run portNum app
